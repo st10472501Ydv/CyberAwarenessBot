690e25d91f4d6c346ef4cbc3091bcac96a9698ef
st10472501Ydv <st10472501@myemeris.edu.za>
2026-03-30 13:52:18 +0200

    Initial commit: Set up project structure and ASCII art display

---
+++ CyberAwarenessBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using CyberAwarenessBot.UI;
+
+namespace CyberAwarenessBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AsciiArt.DisplayLogo();
+        }
+    }
+}
---
+++ CyberAwarenessBot/src/UI/AsciiArt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberAwarenessBot.UI
+{
+    public static class AsciiArt
+    {
+        public static void DisplayLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine(@"   ___     _                               ");
+            Console.WriteLine(@"  / __|  _| |__  ___ _ _                  ");
+            Console.WriteLine(@" | (_| || | '_ \/ -_) '_|                 ");
+            Console.WriteLine(@"  \___\_, |_.__/\___|_|                   ");
+            Console.WriteLine(@"   /_\|__/_ ____ _ _ _ ___ _ _  ___ ______");
+            Console.WriteLine(@"  / _ \ V  V / _` | '_/ -_) ' \/ -_|_-<_-<");
+            Console.WriteLine(@" /_/_\_\_/\_/\__,_|_| \___|_||_\___/__/__/ ");
+            Console.WriteLine(@" | _ ) ___| |_                             ");
+            Console.WriteLine(@" | _ \/ _ \  _|                            ");
+            Console.WriteLine(@" |___/\___/\__|                            ");
+
+            Console.ResetColor();
+        }
+    }
+}
